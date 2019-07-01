aa547996b384808de773a3bc2294130f3035f4fb
Settybo_rong <settybo_rong@SettyboongdeAir.lan>
2019-07-01 19:02:08 +0800

    线性动画提交

--- lib/USER/animation.dart
+++ lib/USER/animation.dart
@@ -19,18 +19,28 @@ class animationDemo extends StatefulWidget{
 
 class animationState extends State<animationDemo> with TickerProviderStateMixin{
   AnimationController animationDemoController;
+  Animation animation;
+  Animation animationColor;
+  CurvedAnimation curve;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
 
     animationDemoController = AnimationController(
-      value: 32,
+
+//      lowerBound: 32,
+//      value: 32,
         vsync: this,
 //        lowerBound: 0.0,
-        upperBound: 100.0,
-        duration: Duration(milliseconds: 3000));
+//        upperBound: 100.0,
+        duration: Duration(milliseconds: 1000));
 
+    curve = CurvedAnimation(parent: animationDemoController, curve: Curves.bounceOut);
+    animation =  Tween(begin: 32.0,end: 100.0).animate(curve);
+    animationColor = ColorTween(begin: Colors.red,end: Colors.red[900]).animate(animationDemoController);
+
+    
     animationDemoController.addListener((){
       print("${animationDemoController.value}");
       setState(() {
@@ -58,14 +68,43 @@ class animationState extends State<animationDemo> with TickerProviderStateMixin{
   Widget build(BuildContext context) {
     // TODO: implement build
     return Center(
-      
-      child: IconButton(
-          iconSize: animationDemoController.value,
-          icon:Icon(Icons.favorite),
-          onPressed: (){
+      child: AnimateHeart(
+        animations: [
+          animationColor,
+          animation
+        ],
+        controller: animationDemoController,
+      ),
 
-            animationDemoController.forward();
-          }),
     );
+  }
+}
+
+class AnimateHeart extends AnimatedWidget{
+  final List animations;
+  final AnimationController controller;
+
+  AnimateHeart({
+    this.animations,this.controller
+  }) : super(listenable : controller);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return  IconButton(
+        iconSize: animations[1].value,
+        color: animations[0].value,
+        icon:Icon(Icons.favorite),
+
+        onPressed: (){
+          switch (controller.status){
+            case AnimationStatus.completed:
+              controller.reverse();
+              break;
+            default:
+              controller.forward();
+              break;
+          }
+        });
   }
 }
