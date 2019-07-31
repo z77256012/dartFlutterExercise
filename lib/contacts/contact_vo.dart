ebce725ec351d0547f27a355bb8ce12985559d42
Settybo_rong <settybo_rong@SettyboongdeAir.lan>
2019-07-31 23:00:03 +0800

    修改更新

--- lib/contacts/contact_vo.dart
+++ lib/contacts/contact_vo.dart
@@ -20,7 +20,7 @@ List<ContactVo> contactData = [
       name: '波波',
       avatarUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559156255105&di=c1e259434e0c52e3f95a1f4d02e0b091&imgtype=0&src=http%3A%2F%2Fs8.sinaimg.cn%2Fmw690%2F0020go8vgy6OqnF3a2Xf7%26690'),
   ContactVo(seationKey: 'C',
-      name: '乘物元',
+      name: '乘物',
       avatarUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559156255105&di=c1e259434e0c52e3f95a1f4d02e0b091&imgtype=0&src=http%3A%2F%2Fs8.sinaimg.cn%2Fmw690%2F0020go8vgy6OqnF3a2Xf7%26690'),
   ContactVo(seationKey: 'C',
       name: '陈兽 ',
